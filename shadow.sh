fae493621c85f40bd2327236ed080aa7dcfaab24
hidevvz <95039331+hidevvz@users.noreply.github.com>
2023-09-22 07:47:00 +0500

    Delete shadow.sh

--- shadow.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-echo "Enter Proxy UserName :"
-read $proxyUserName
-echo "Enter Proxy Password :"
-read $proxyPassword
-sudo apt-get update
-echo "proxy username is $proxyUserName and proxy password i s $proxyPassword"
